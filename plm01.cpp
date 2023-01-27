1b0f3f92254d85b6a50d68cc41a35061d4d32a94
Shapin199 <mdtanvir054321@gmail.com>
2023-01-27 10:36:44 +0600

    add plm 1

---
+++ plm01.cpp
@@ -0,0 +1,21 @@
+/**
+ *    author:  Mohammad Irtisum
+ *    created: 27.01.2023        
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Student 
+{
+   public:
+   string name;
+   int roll_no;
+};
+int main()
+{
+   Student shapin;
+   shapin.name = "Jamshed";
+   shapin.roll_no = 2;
+   cout<<shapin.name<<endl;
+   cout<<shapin.roll_no<<endl;
+}
